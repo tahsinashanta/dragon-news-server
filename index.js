6075153b46b6c4e824e08b96bbe8a3a62f562ca0
tahsinashanta <tahsinashanta@gmail.com>
2022-10-19 20:41:02 +0600

    basic backend server for dragon news

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require ('express');
+const app = express();
+const cors = require ('cors'); 
+const port = process.env.PORT || 5000;
+
+const categories = require('./data/categories.json'); 
+
+app.use(cors())
+
+app.get('/', (req, res) => {
+    res.send('News API running')
+});
+
+app.get('/news-categories', (req, res)=> {
+    res.send(categories)
+})
+
+app.listen(port, () => {
+    console.log(`Dragon News Server running on ${port}`);
+})
